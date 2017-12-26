5d86624ce9df54e4f1ff2690b10da6e8d23e8669
duruofei <120278473@qq.com>
2017-12-26 21:59:07 +0800

    no security

---
+++ src/main/java/com/drf/springboot/config/Inter.java
@@ -0,0 +1,50 @@
+package com.drf.springboot.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class Inter extends WebMvcConfigurerAdapter
+{
+	@Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        // 默认语言
+        slr.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        // 参数名
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    @Bean
+	public MessageSource messageSource()
+	{
+		ReloadableResourceBundleMessageSource r = new ReloadableResourceBundleMessageSource();
+		r.setFallbackToSystemLocale(true);
+		Locale.setDefault(Locale.SIMPLIFIED_CHINESE );
+		r.setBasename("classpath:i18n/lang");
+		//是否持久缓存
+		r.setCacheSeconds(-1);
+		return r;
+	};
+}
